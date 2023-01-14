662fedf23ea475b048cffb1615fa5317e2b0ebcc
AleVerv <122140934+AleVerv@users.noreply.github.com>
2023-01-14 02:00:41 -0800

    Update second program.py

--- lesson 2/second program.py
+++ lesson 2/second program.py
@@ -2,7 +2,7 @@ name = input("Введите имя " )
 print("Привет", name)
 
 
-x = int(input("Введите 1е число"))
-y = int(input("Введите 2е число"))
+x = int(input("Введите 1е число "))
+y = int(input("Введите 2е число "))
 
 print("Сумма", x + y)
