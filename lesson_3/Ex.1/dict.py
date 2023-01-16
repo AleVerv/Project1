05f2723bd4636bc59ddad8fb6762864738dcd5e9
Alexa <teaflower17@gmail.com>
2023-01-15 21:22:19 -0800

    Работа с байтами

---
+++ lesson_3/Ex.1/dict.py
@@ -0,0 +1,14 @@
+student = {'name' : 'John', 'age' : 25, 'products' : ['apple', 'banana', 'peach', 'pear']}
+#print(student)
+#print(student['name'])
+#student['name'] = 'Michael'
+#del student['age']
+#age = student.pop('age')
+#print(age)
+#print(len(student))
+#print(student.items())
+values = student.values()
+print(values)
+
+#student['phone'] = '555-55-5555'
+#print(student)
---
+++ lesson_3/Ex.1/tuple.py
@@ -0,0 +1,20 @@
+tuple_ = (4, 5, 6, 7, 10, 12)
+print(tuple_)
+print(type(tuple_))
+print(tuple_[0])
+a = (1, 2, 3, 5, 7, 8)
+b = tuple(a)
+print(b)
+print(type(b))
+
+d = [1, 2, 3, 5, 7, 8]
+e = tuple(d)
+print(e)
+print(type(e))
+
+tuple2 = [(4, 5, 6, 7, 10, 12), (1, 2, 3, 5, 1,)]
+print(tuple2)
+
+student = ('Josh', 21, 'male')
+print(student.count('Josh'))
+print(student.index('male'))
